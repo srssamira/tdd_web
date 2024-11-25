2af15e896e41376bb78b717dcc8c7b8a62b8c772
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-11-25 11:01:28 -0300

    First commit

---
+++ src/main/java/br/com/zup/tdd_web/TddWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.tdd_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/tdd_web/TddWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.tdd_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
